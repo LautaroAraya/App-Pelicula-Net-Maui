a7a044e3fc72bceeecbc8c5366ae52289c8f7eba
Lautaro <lautyaraya65@gmail.com>
2023-09-29 15:01:06 -0300

    Agregar archivos de proyecto.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿namespace AppPelicula
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void AlPresionarBoton(object sender, EventArgs e)
+        {
+            count++;
+
+            if (count == 1)
+                CounterBtn.Text = $"Presionado {count} vez";
+            else
+                CounterBtn.Text = $"Presionad {count} veces";
+
+            SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+    }
+}
